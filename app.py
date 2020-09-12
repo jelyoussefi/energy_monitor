e7599922e2ccf7d2bf8e44bf440989fa6b752f5c
Jamal El Youssefi <jamal.elyoussefi@yahoo.fr>
2020-09-12 17:15:42 +0200

    add config file

--- app.py
+++ app.py
@@ -2,101 +2,102 @@ from datetime import datetime, timedelta
 import time
 from threading import Condition
 from flask import Flask, Response, json, request, render_template, jsonify
-from flask import Markup
 from flaskext.mysql import MySQL
-#from werkzeug import generate_password_hash, check_password_hash
 from flask import session
 
 
-mysql = MySQL()
-app = Flask(__name__)
-app.config['SESSION_TYPE'] = 'memcached'
-app.secret_key = 'My secret key?'
+class EnergyMonitor():
+	def __init__(self, config_path=None):
+		with open('./config.js') as json_file:
+			self.config = json.load(json_file)
+		
+		self.cv = Condition()
 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'jamal'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Jamal$1969'
-app.config['MYSQL_DATABASE_DB'] = 'awahid_db'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+		self.mysql = MySQL()
+		self.app = Flask(__name__)
+		self.app.config['SESSION_TYPE'] = 'memcached'
+		self.app.secret_key = 'My secret key?'
 
-startTime = None
-interval = 5*60
-cv = Condition()
+		self.app.config['MYSQL_DATABASE_USER'] = self.config['user']
+		self.app.config['MYSQL_DATABASE_PASSWORD'] = self.config['password']
+		self.app.config['MYSQL_DATABASE_DB'] = self.config['database']
+		self.app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+		self.mysql.init_app(self.app)
 
+		self.conn = self.mysql.connect()
+		self.cursor = self.conn.cursor()
+		self.cursor.execute("SELECT * FROM `"+self.config['table']+"`")
+		rows = self.cursor.fetchall()
+		self.startTime = rows[0][1]
+		self.interval = 5*60
 
-@app.route('/')
-def index():
-	return render_template('index.html')
+		self.cursor.execute("SHOW columns FROM `energie10`")
+		self.columns = self.cursor.fetchall()
 
-@app.route('/data')
-def data():
-	return Response(dataHandler(), mimetype='text/event-stream')
+	def start(self):
+		app = self.app
+		@app.route('/')
+		def index():
+			return render_template('index.html')
 
-@app.route('/handle_command', methods = ['POST'])
-def handle_command():
-	if request.method == 'POST':
-		global startTime, interval, cv
-		cv.acquire()
-		cmd = request.get_json()
-		if cmd['type'] == 'scroll_left':
-			startTime -= timedelta(seconds=interval)
-		elif cmd['type'] == 'scroll_right':
-			startTime += timedelta(seconds=interval)
+		@app.route('/data')
+		def data():
+			return Response(self.dataHandler(), mimetype='text/event-stream')
 
-		cv.notify()
-		cv.release()
+		@app.route('/handle_command', methods = ['POST'])
+		def handle_command():
+			if request.method == 'POST':
+				self.cv.acquire()
+				
+				cmd = request.get_json()
+				if cmd['type'] == 'scroll_left':
+					self.startTime -= timedelta(seconds=self.interval)
+				elif cmd['type'] == 'scroll_right':
+					self.startTime += timedelta(seconds=self.interval)
+				elif cmd['type'] == 'interval':
+					self.interval = cmd['value']
 
-	return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+				self.cv.notify()
+				self.cv.release()
 
+			return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
-def getData(cursor, startTime, interval):
-	endTime = startTime +  timedelta(seconds=interval)
-	print("\n\t Interval {} -> {}\n".format(startTime, endTime))
-	cursor.execute("SELECT * FROM `energie10` WHERE date between timestamp \""+ str(startTime) + "\" and timestamp \""+str(endTime)+"\"")
-	return cursor.fetchall()
+		self.app.run(host='0.0.0.0', port=str(self.config['port']), threaded=True)
 
-def dataHandler(): 
-	global startTime, interval, cv
-	conn = mysql.connect()
-	cursor = conn.cursor()
+	def getData(self):
+		endTime = self.startTime +  timedelta(seconds=self.interval)
+		print("\n\t Interval {} -> {}\n".format(self.startTime, endTime))
+		self.cursor.execute("SELECT * FROM `"+self.config['table']+"` WHERE date between timestamp \""+ str(self.startTime) + "\" and timestamp \""+str(endTime)+"\"")
+		return self.cursor.fetchall()
 
-	#yield f"data:{json.dumps({'reset': 'on'})}\n\n"
-	cursor.execute("SELECT * FROM `energie10`")
-	rows = cursor.fetchall()
-	startTime = rows[0][1]
+	def dataHandler(self): 
+		self.cv.acquire()
+		while True:
+			data = {}
+			print("\n--------------------------> Processing New Data ... ")
+			rows = self.getData()
 
-	cursor.execute("SHOW columns FROM `energie10`")
+			labels = list()
+			for row in rows:
+				labels.append(row[1])
 
-	columns = cursor.fetchall()
+			data['labels'] = labels;
+			data['datasets'] = []
+			for c in range(2, len(self.columns)):
+				data['datasets'].append({'label':self.columns[c][0], 'data': []})
 
-	while True:
-		cv.acquire()
-		data = {}
-		print("\n--------------------------> Processing New Data ... ")
-		rows = getData(cursor, startTime, interval)
-		labels = list()
-		for row in rows:
-			labels.append(row[1])
-
-		data['labels'] = labels;
-		data['datasets'] = []
-		for c in range(2, len(columns)):
-			data['datasets'].append({'label':columns[c][0], 'data': []})
-
-		for row in rows:
-			for c in range(2, len(columns)):
-				value = row[c];
-				data['datasets'][c-2]['data'].append(value)
-       	
-		yield f"data:{json.dumps(data)}\n\n"
-		print("--------------------------> New Data submitted ")
-		cv.wait()
-		cv.release()
-
-	cursor.close()
-	conn.close()
-        
+			for row in rows:
+				for c in range(2, len(self.columns)):
+					value = row[c];
+					data['datasets'][c-2]['data'].append(value)
+	       	
+			yield f"data:{json.dumps(data)}\n\n"
+			print("--------------------------> New Data submitted ")
+			self.cv.wait()
+		
+		self.cv.release()
+	        
 
 if __name__ == "__main__":
-    app.run(port=5002)
+    energyMonitor = EnergyMonitor()
+    energyMonitor.start()
---
+++ config.js
@@ -0,0 +1,7 @@
+{
+	"user": "jamal",
+	"password": "Jamal$1969",
+	"database": "awahid_db",
+	"table": "energie10",
+	"port": 5000
+}
