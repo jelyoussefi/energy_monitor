93ebcab7f25149a8037110ad760094aadda71cf1
Jamal El Youssefi <jamal.elyoussefi@yahoo.fr>
2021-02-03 16:44:11 +0100

    update

---
+++ config.abd.js
@@ -0,0 +1,16 @@
+{
+	"user": "request",
+	"password": "request",
+	"database": "request",
+	"table": "energie10",
+	"server": "localhost",
+	"max_samples": 64,
+	"port": 5000,
+	"scales": {"U": 100},
+	"refresh_time": 5,
+	"k": 0.1,
+	"envoy": {
+			"ip": "192.168.1.106",
+			"sid": 123456
+	}		
+}
